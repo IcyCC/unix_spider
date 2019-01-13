9aed4716daa927ae37b02cc2d71a679a4eafe620
suchang <1103232282@qq.com>
2019-01-13 23:13:21 +0800

    处理

--- src/util.h
+++ src/util.h
@@ -105,7 +105,7 @@ inline std::list<char> String2List(std::string s) {
     return res;
 }
 
-inline  std::string DropTag(std::string s){
+inline std::string DropTag(std::string s) {
     auto l = String2List(s);
     std::vector<std::list<char>::iterator> sure_delete_stack;
     enum DropTagStatus {
@@ -114,15 +114,15 @@ inline  std::string DropTag(std::string s){
     };
 
     DropTagStatus status = DropTagStatus::NONE;
-    for (auto it = l.begin(); it != l.end(); it++){
+    for (auto it = l.begin(); it != l.end(); it++) {
         char item = *it;
-        if (item == '<'){
+        if (item == '<') {
             status = DropTagStatus::TAG;
         }
-        if (status == DropTagStatus::TAG){
+        if (status == DropTagStatus::TAG) {
             sure_delete_stack.push_back(it);
         }
-        if (item == '>'){
+        if (item == '>') {
             status = DropTagStatus::NONE;
         }
     }
@@ -134,20 +134,19 @@ inline  std::string DropTag(std::string s){
     return List2String(l);
 }
 
-inline std::string Gethost(std::string url)
-{
-    size_t pos=url.find("://");
-    if(pos!=url.npos)
-        url=url.substr(pos+3);
-    pos=url.find("/");
-    if(pos==url.npos)
-        cout<<url;
-    else
-    url=url.substr(0,pos);
+inline std::string Gethost(std::string url) {
+    size_t pos = url.find("://");
+    if (pos != url.npos)
+        url = url.substr(pos + 3);
+    pos = url.find("/");
+    if (pos == url.npos) {
+
+    } else
+        url = url.substr(0, pos);
     return url;
 }
 
-inline std::map<std::string, std::string> ParseHttpHeader(const std::string &raw){
+inline std::map<std::string, std::string> ParseHttpHeader(const std::string &raw) {
     return std::map<std::string, std::string>();
 }
 
@@ -177,18 +176,18 @@ inline std::pair<int, int> FindMaxTag2WordPos(const std::string &text, int start
         }
         if (offset == 0) {
             tag_sum[offset] = tag_flag ? 1 : 0;
-            word_sum[(end_pos-start_pos)-offset] = tag_flag ? 0 : 1;
+            word_sum[(end_pos - start_pos) - offset] = tag_flag ? 0 : 1;
         } else {
             tag_sum[offset] = tag_sum[offset - 1] + tag_flag ? 1 : 0;
-            word_sum[(end_pos-start_pos)-offset] = word_sum[(end_pos-start_pos)-offset - 1] + tag_flag ? 0 : 1;
+            word_sum[(end_pos - start_pos) - offset] = word_sum[(end_pos - start_pos) - offset - 1] + tag_flag ? 0 : 1;
         }
     }
 
     int top_offset = 0;
     int top_value = tag_sum[0] + word_sum[0];
-    for (int i = 1; i < offset-1; i++) {
+    for (int i = 1; i < offset - 1; i++) {
         int v = tag_sum[i] + word_sum[i];
-        if (v > top_value){
+        if (v > top_value) {
             top_offset = i;
             top_value = v;
         }
@@ -224,18 +223,18 @@ inline std::pair<int, int> FindMaxWord2TagPos(const std::string &text, int start
         }
         if (offset == 0) {
             word_sum[offset] = tag_flag ? 1 : 0;
-            tag_sum[(end_pos-start_pos)-offset] = tag_flag ? 0 : 1;
+            tag_sum[(end_pos - start_pos) - offset] = tag_flag ? 0 : 1;
         } else {
             word_sum[offset] = word_sum[offset - 1] + tag_flag ? 1 : 0;
-            tag_sum[(end_pos-start_pos)-offset] = tag_sum[(end_pos-start_pos)-offset - 1] + tag_flag ? 0 : 1;
+            tag_sum[(end_pos - start_pos) - offset] = tag_sum[(end_pos - start_pos) - offset - 1] + tag_flag ? 0 : 1;
         }
     }
 
     int top_offset = 0;
     int top_value = tag_sum[0] + word_sum[0];
-    for (int i = 1; i < offset-1; i++) {
+    for (int i = 1; i < offset - 1; i++) {
         int v = tag_sum[i] + word_sum[i];
-        if (v > top_value){
+        if (v > top_value) {
             top_offset = i;
             top_value = v;
         }
@@ -245,65 +244,58 @@ inline std::pair<int, int> FindMaxWord2TagPos(const std::string &text, int start
 }
 
 
-inline std::string Getpath(std::string url)
-{
-	size_t pos = url.find("://");
-	if (pos != url.npos) url = url.substr(pos + 3);
-	pos = url.find('/');
-	if(pos == url.npos)
-		url="/";
-	else
-		url= url.substr(pos);
-	return url;
+inline std::string Getpath(std::string url) {
+    size_t pos = url.find("://");
+    if (pos != url.npos) url = url.substr(pos + 3);
+    pos = url.find('/');
+    if (pos == url.npos)
+        url = "/";
+    else
+        url = url.substr(pos);
+    return url;
 }
-inline std::map<std::string, std::string> ParseHttpHeader(std::string raw)
-{
-	int pos=raw.find("\n");
-	if (pos != raw.npos) raw = raw.substr(pos);
-	char * strc = new char[strlen(raw.c_str())+1];
-	strcpy(strc, raw.c_str());
-	string pattern="\n";
-	vector <string> result;
-	char* tmpStr=strtok(strc,pattern.c_str());
-	while(tmpStr!=NULL)
-	{
-		result.push_back(string(tmpStr));
-		tmpStr=strtok(NULL,pattern.c_str());
-	}
-	map<std::string, std::string> mapPara;
-	for (int i = 0; i < result.size(); i++)
-	{
-	    int pos=result[i].find(":");
-	    if(pos!=result[i].npos)
-	    {
-		   string behind=result[i].substr(pos+1);
-	       string front=result[i].substr(0,pos);
-		   mapPara.insert(pair<std::string, std::string>(front,behind));
-	   	}
-	}
-	delete[] strc ;
-	/*std::map<std::string, std::string>::iterator it;
-	for(it = mapPara.begin(); it != mapPara.end(); it++)
-	        cout << it->first << " " << it->second << endl;*/
-	return mapPara;
+
+inline std::map<std::string, std::string> ParseHttpHeader(std::string raw) {
+    int pos = raw.find("\n");
+    if (pos != raw.npos) raw = raw.substr(pos);
+    char *strc = new char[strlen(raw.c_str()) + 1];
+    strcpy(strc, raw.c_str());
+    std::string pattern = "\n";
+    std::vector<std::string> result;
+    char *tmpStr = strtok(strc, pattern.c_str());
+    while (tmpStr != NULL) {
+        result.push_back(std::string(tmpStr));
+        tmpStr = strtok(NULL, pattern.c_str());
+    }
+    std::map <std::string, std::string> mapPara;
+    for (int i = 0; i < result.size(); i++) {
+        int pos = result[i].find(":");
+        if (pos != result[i].npos) {
+            std::string behind = result[i].substr(pos + 1);
+            std::string front = result[i].substr(0, pos);
+            mapPara.insert(std::pair<std::string, std::string>(front, behind));
+        }
+    }
+    delete[] strc;
+    return mapPara;
 }
-inline std::map<std::string, std::string> ParseUrl(std::string url)
-{
-	map<std::string, std::string> mapUrl;
-	int pos=url.find("://")	;
-	string protocol;
-	if(pos!=url.npos)
-		protocol=url.substr(0,pos);
-	mapUrl.insert(pair<std::string, std::string>("protocol",protocol));
-	url=url.substr(pos+3);
-	string domain;
-	pos=url.find("/");
-	domain=url.substr(0,pos);
-	mapUrl.insert(pair<std::string, std::string>("domain",domain));
-	url=url.substr(pos+1);
-	mapUrl.insert(pair<std::string, std::string>("path",url));
-	/*std::map<std::string, std::string>::iterator it;
-		for(it = mapUrl.begin(); it != mapUrl.end(); it++)
-		        cout << it->first << " " << it->second << endl;*/
-	return mapUrl;
+
+inline std::map<std::string, std::string> ParseUrl(std::string url) {
+    std::map <std::string, std::string> mapUrl;
+    int pos = url.find("://");
+    std::string protocol;
+    if (pos != url.npos)
+        protocol = url.substr(0, pos);
+    mapUrl.insert(std::pair<std::string, std::string>("protocol", protocol));
+    url = url.substr(pos + 3);
+    std::string domain;
+    pos = url.find("/");
+    domain = url.substr(0, pos);
+    mapUrl.insert(std::pair<std::string, std::string>("domain", domain));
+    url = url.substr(pos + 1);
+    mapUrl.insert(std::pair<std::string, std::string>("path", url));
+    /*std::map<std::string, std::string>::iterator it;
+        for(it = mapUrl.begin(); it != mapUrl.end(); it++)
+                cout << it->first << " " << it->second << endl;*/
+    return mapUrl;
 }
